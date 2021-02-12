15e0a8efb9534375c6bf0529cb894be03757eb87
Оксана Анощенко <anoschenko.ov@phystech.edu>
2021-02-12 18:46:38 +0300

    add eslint and prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-import '../styles/App.css';
+import React from 'react'
+import '../styles/App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        hello
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
